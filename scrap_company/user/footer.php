e2ec613e08d62f34562dfcc637da8490925bf760
antovargheseantony <77603957+antovargheseantony@users.noreply.github.com>
2022-05-05 14:43:15 +0530

    Update footer.php

--- scrap_company/user/footer.php
+++ scrap_company/user/footer.php
@@ -7,7 +7,7 @@
       <footer class="sticky-footer bg-white">
         <div class="container my-auto">
           <div class="copyright text-center my-auto">
-            <span>Copyright &copy; Your CareCycle 2022</span>
+            <span>Copyright &copy; Your VScrap 2022</span>
           </div>
         </div>
       </footer>
