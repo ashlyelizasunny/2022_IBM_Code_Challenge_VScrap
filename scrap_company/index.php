c5bb2580e1c89f3fab8498ee1fb8848555727e0e
antovargheseantony <77603957+antovargheseantony@users.noreply.github.com>
2022-05-05 15:54:04 +0530

    Add files via upload

---
+++ scrap_company/index.php
@@ -0,0 +1,244 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>VScrap</title>
+  <meta content="" name="descriptison">
+  <meta content="" name="keywords">
+
+  <!-- Favicons -->
+  <link href="assets/img/favicon.png" rel="icon">
+  <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="assets/vendor/icofont/icofont.min.css" rel="stylesheet">
+  <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="assets/vendor/owl.carousel/assets/owl.carousel.min.css" rel="stylesheet">
+  <link href="assets/vendor/venobox/venobox.css" rel="stylesheet">
+  <link href="assets/vendor/aos/aos.css" rel="stylesheet">
+
+  <!-- Template Main CSS File -->
+  <link href="assets/css/style.css" rel="stylesheet">
+
+  <!-- =======================================================
+  * Template Name: Appland - v2.2.0
+  * Template URL: https://bootstrapmade.com/free-bootstrap-app-landing-page-template/
+  * Author: BootstrapMade.com
+  * License: https://bootstrapmade.com/license/
+  ======================================================== -->
+</head>
+
+<body>
+
+  <!-- ======= Header ======= -->
+  <header id="header" class="fixed-top  header-transparent ">
+    <div class="container d-flex align-items-center">
+
+      <div class="logo mr-auto">
+
+        <!-- Uncomment below if you prefer to use an image logo -->
+         <a href="index.php"><img src="" alt="" class="img-fluid"></a>
+      </div>
+
+      <nav class="nav-menu d-none d-lg-block">
+        <ul>
+          <li class="active"><a   href="index.php">Home</a></li>
+
+          <li><a   href="#faq">F.A.Q</a></li>
+          <li><a   href="#contact">Contact Us</a></li>
+
+        </ul>
+      </nav><!-- .nav-menu -->
+
+    </div>
+  </header><!-- End Header -->
+
+  <!-- ======= Hero Section ======= -->
+  <section id="hero" class="d-flex align-items-center">
+
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-6 d-lg-flex flex-lg-column justify-content-center align-items-stretch pt-5 pt-lg-0 order-2 order-lg-1" data-aos="fade-up">
+          <div>
+            <h1>Scrappage Policy Automation</h1>
+            <h2>  ”Scrappage Policy Automation” is a web
+application that allows the user to register for scrappage of
+their vehicles.</h2>
+            <a href="login/register.php" class="download-btn"><i class="bx bxl-play-store"></i> Register</a>
+            <a href="login/login.php" class="download-btn"><i class="bx bxl-play-store"></i> Login</a>
+          </div>
+        </div>
+        <div class="col-lg-6 d-lg-flex flex-lg-column align-items-stretch order-1 order-lg-2 hero-img" data-aos="fade-up">
+          <img src="assets/img/hero-img1.png" class="img-fluid" alt="">
+        </div>
+      </div>
+    </div>
+
+  </section><!-- End Hero -->
+
+  <main id="main">
+
+    <!-- ======= App Features Section ======= -->
+    <section id="features" class="features">
+      <div class="container">
+
+        <div class="section-title">
+          <h2>App Features</h2>
+          <p>”Scrappage Policy Automation” is a real time project,which is
+useful for both the employees as well as for the public who are
+facing problems with the current manual work.
+.</p>
+        </div>
+
+
+      </div>
+    </section><!-- End App Features Section -->
+
+
+
+    <!-- ======= Frequently Asked Questions Section ======= -->
+    <section id="faq" class="faq section-bg">
+      <div class="container">
+
+        <div class="section-title">
+
+          <h2>Frequently Asked Questions</h2>
+          <p>The scrappage policy launched on August 13th 2021,is a
+government founded programme to replace old vehicles to
+new and modern ones.
+.</p>
+        </div>
+
+        <div class="accordion-list">
+          <ul>
+            <li data-aos="fade-up">
+              <i class="bx bx-help-circle icon-help"></i> <a data-toggle="collapse" class="collapse" href="#accordion-list-1">WHAT EXACTLY IS VEHICLE SCRAPPAGE POLICY? <i class="bx bx-chevron-down icon-show"></i><i class="bx bx-chevron-up icon-close"></i></a>
+              <div id="accordion-list-1" class="collapse show" data-parent=".accordion-list">
+                <p>
+               Much like the western countries, the scrappage policy comes into effect when a vehicle’s registration is complete. In general, a passenger vehicle has a life of 15 years and a commercial vehicle has a life of 10 years, after which they become obsolete and also starts polluting the environment at a greater intensity than they would have been doing earlier. In western countries, these old vehicles are sent to scrapyards where they are dismantled and the steel used for making the body is crushed and recycled again. In India though, there was no such policy. Most of the vehicles either are on run currently polluting the environment or are lying at road sides.  </p>
+              </div>
+            </li>
+
+            <li data-aos="fade-up" data-aos-delay="100">
+              <i class="bx bx-help-circle icon-help"></i> <a data-toggle="collapse" href="#accordion-list-2" class="collapsed">WHAT DOES THE POLICY AIM TO ACHIEVE? <i class="bx bx-chevron-down icon-show"></i><i class="bx bx-chevron-up icon-close"></i></a>
+              <div id="accordion-list-2" class="collapse" data-parent=".accordion-list">
+                <p>
+             As explained above, a vehicle should be scrapped at the end of its lifecycle so that they are stopped running on roads that will reduce air pollution. Also, getting off older vehicles will generate space for new vehicles, which will boost the sales in the otherwise battered and bruised Indian auto industry. Nitin Gadkari explained that once the registration of the vehicles are over, they will have to go mandatory fitness test. However, they can also avail scrapping option.    </p>
+              </div>
+            </li>
+
+            <li data-aos="fade-up" data-aos-delay="200">
+              <i class="bx bx-help-circle icon-help"></i> <a data-toggle="collapse" href="#accordion-list-3" class="collapsed">WILL ALL VEHICLES OVER AND ABOVE TIME LIMIT WILL BE SCRAPPED? <i class="bx bx-chevron-down icon-show"></i><i class="bx bx-chevron-up icon-close"></i></a>
+              <div id="accordion-list-3" class="collapse" data-parent=".accordion-list">
+                <p>
+               No, not all vehicles will be scrapped as this is a voluntary scheme and not a mandatory one. However, there’s a catch here. All vehicles over and above the time limit will have to undergo mandatory fitness test. If a vehicles fails fitness test, they will not get renewal certificate and won’t be able to run on road. However, if they pass fitness test, they will have to undergo fitness test after every 5 years again to show their road worthiness.  </p>
+              </div>
+            </li>
+
+            <li data-aos="fade-up" data-aos-delay="300">
+              <i class="bx bx-help-circle icon-help"></i> <a data-toggle="collapse" href="#accordion-list-4" class="collapsed">WHAT BENEFIT WILL I GET FROM SCRAPPING MY VEHICLE? <i class="bx bx-chevron-down icon-show"></i><i class="bx bx-chevron-up icon-close"></i></a>
+              <div id="accordion-list-4" class="collapse" data-parent=".accordion-list">
+                <p>
+              Nitin Gadkari has given following financial benefits one can get from the policy-
+<br>
+1) A scrap value of the ex-showroom price of vehicle ranging from 4-6% will be given to the owner if they choose to scrap the vehicle
+<br>
+2) A rebate of upto 25% will be given in Road Tax
+<br>
+3) Vehicle manufacturers will be advised to give 5% discount on new vehicles against a scrapping certificate
+<br>
+4) No registration fees for vehicles.   </p>
+              </div>
+            </li>
+
+
+
+          </ul>
+        </div>
+
+      </div>
+    </section><!-- End Frequently Asked Questions Section -->
+
+    <!-- ======= Contact Section ======= -->
+    <section id="contact" class="contact">
+      <div class="container">
+
+        <div class="section-title">
+          <h2>Contact</h2>
+         </div>
+
+        <div class="row">
+
+          <div class="col-lg-12">
+            <div class="row">
+              <div class="col-lg-3 info" data-aos="fade-up">
+                <i class="bx bx-map"></i>
+                <h4>Address</h4>
+                <p>RIT<br>Kottayam</p>
+              </div>
+              <div class="col-lg-3 info" data-aos="fade-up" data-aos-delay="100">
+                <i class="bx bx-phone"></i>
+                <h4>Call Us</h4>
+                <p>+919645961799</p>
+              </div>
+              <div class="col-lg-3 info" data-aos="fade-up" data-aos-delay="200">
+                <i class="bx bx-envelope"></i>
+                <h4>Email Us</h4>
+                <p>contact@example.com<br>info@example.com</p>
+              </div>
+              <div class="col-lg-3 info" data-aos="fade-up" data-aos-delay="300">
+                <i class="bx bx-time-five"></i>
+                <h4>Working Hours</h4>
+                <p>Mon - Fri: 9AM to 5PM<br></p>
+              </div>
+            </div>
+          </div>
+
+
+        </div>
+
+      </div>
+    </section><!-- End Contact Section -->
+
+  </main><!-- End #main -->
+
+  <!-- ======= Footer ======= -->
+  <footer id="footer">
+
+
+
+
+    <div class="container py-4">
+      <div class="copyright">
+        &copy; Copyright <strong><span>VScrap</span></strong>. All Rights Reserved
+      </div>
+      <div class="credits">
+          Designed by <a href="#">Anto Varghese, Abhinandh N K, Harikrishnan Nair K G, Ashly Elizabeth Sunny, Karthik Suresh</a>
+      </div>
+    </div>
+  </footer><!-- End Footer -->
+
+  <a href="#" class="back-to-top"><i class="icofont-simple-up"></i></a>
+
+  <!-- Vendor JS Files -->
+  <script src="assets/vendor/jquery/jquery.min.js"></script>
+  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="assets/vendor/jquery.easing/jquery.easing.min.js"></script>
+  <script src="assets/vendor/php-email-form/validate.js"></script>
+  <script src="assets/vendor/owl.carousel/owl.carousel.min.js"></script>
+  <script src="assets/vendor/venobox/venobox.min.js"></script>
+  <script src="assets/vendor/aos/aos.js"></script>
+
+  <!-- Template Main JS File -->
+  <script src="assets/js/main.js"></script>
+
+</body>
+
+</html>
